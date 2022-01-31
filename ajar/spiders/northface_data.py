0ce99afa4f23154f17aeca545c3a1b050e9e9582
raja1802 <rajagandla16@gmail.com>
2022-01-31 11:29:47 +0530

    northface data collection

---
+++ ajar/spiders/northface_data.py
@@ -0,0 +1,134 @@
+import scrapy
+from ajar.items import AmazonUs
+from scrapy.spiders import CrawlSpider, Rule, SitemapSpider
+from selenium import webdriver
+from time import sleep
+from scrapy.selector import Selector
+import json
+import os
+from scrapy.linkextractors import LinkExtractor as sle
+from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+from fake_useragent import UserAgent
+ua = UserAgent()
+# chrome requirments
+GOOGLE_CHROME_PATH = "/app/.apt/usr/bin/google-chrome"
+CHROMEDRIVER_PATH = "/app/.chromedriver/bin/chromedriver"
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_argument("--headless")
+chrome_options.add_argument("--no-sandbox")
+chrome_options.add_argument("--incognito")
+chrome_options.add_argument("--disable-plugins-discovery")
+chrome_options.binary_location = GOOGLE_CHROME_PATH
+
+
+class QuotesInfiniteScrollSpider(CrawlSpider):
+    name = "northface_products"
+    rotate_user_agent = True
+    allowed_domains = ["www.thenorthface.com"]
+    start_urls = []
+    # rules = (Rule(sle(allow="/p/", deny=("/c/", )), callback="parse_result", follow=True),)
+    #sitemap_urls = ["https://www.pantaloons.com/product-sitemap.xml"]
+    # parsing results with below function 
+    def parse(self, response):
+        amazon = []
+        amazon = AmazonUs()
+        userAgent = ua.random
+        # chrome_options.add_argument('--proxy-server=%s' % PROXY)
+        chrome_options.add_argument(f'user-agent={userAgent}')
+        browser = webdriver.Chrome(
+            executable_path=os.environ.get("CHROMEDRIVER_PATH"),
+            chrome_options=chrome_options,
+        )
+        browser.get(response.url)
+        sleep(0.2)
+        scrapy_selector = Selector(text=browser.page_source)
+        # css selection of html data tags
+        product_id = scrapy_selector.css(
+            'head > link[rel= "canonical"]::attr(href)'
+        ).get()
+        product_mrp = (
+            scrapy_selector.css(
+                "#product-info > div.product-content-info-price.product-price.product-price-js > span.product-content-info-offer-price.offer-price.offer-price-js.product-price-amount-js::text"
+            ).get()
+            or scrapy_selector.css("span.product-price-amount-js::text").get()
+        )
+        product_description = (
+            scrapy_selector.css(
+                "#productDescriptionAccordion > div.mol-accordion__body > div > section > div > div > div > div > div.desc-container::text"
+            ).getall()
+               )
+        product_name = (
+            scrapy_selector.css(
+                "#product-info > h1.product-content-info-name::text"
+            ).getall()
+              )
+        product_ASIN = scrapy_selector.css(
+            'head > link[rel= "canonical"]::attr(href)'
+        ).get()
+        product_by_url = "www.thenorthface.com"
+        product_by_name = "thenorthface"
+        product_rating = "NA"
+        product_image = (
+            scrapy_selector.css(
+                "head > meta[property='og:image']::attr(content)"
+            ).get()
+            or scrapy_selector.css("head > meta[name='twitter:image']::attr(content)").get()
+        )
+        product_image_2 = (
+            scrapy_selector.css(
+                "#overlay-unique-id-image::attr(data-preview-image-uri)"
+            ).getall()
+        )
+        product_image_3 = (
+            scrapy_selector.css(
+                "#slick-slide82 > img::attr(src)"
+            ).getall()
+        )
+        product_image_4 = (
+            scrapy_selector.css(
+                "#slick-slide83 > img::attr(src)"
+            ).get()
+        )
+        product_price = (
+            scrapy_selector.css("#product-info > div.product-content-info-price.product-price.product-price-js > span.product-content-info-offer-price.offer-price.offer-price-js.product-price-amount-js::text").get()
+        )
+        product_about = (
+            scrapy_selector.css(
+                "#productFeaturesAccordion > div.mol-accordion__body > div > section > div > div > ul > li::text"
+            ).getall()
+        )
+        product_keywords = "northface, shirts, outdoor, branded"
+        product_catlog = "www.thenorthface.com"
+        product_price_2 = (
+            scrapy_selector.css(
+                "span.product-price-amount-js::text"
+            ).getall()
+        )
+
+        product_keywords_2 = "northface, shirts, outdoor, branded"
+
+        # append data to items
+        amazon["product_id"] = product_id
+        amazon["product_mrp"] = product_mrp
+        amazon["product_description"] = product_description
+        amazon["product_name"] = product_name
+        amazon["product_ASIN"] = product_ASIN
+        amazon["product_by_url"] = product_by_url
+        amazon["product_by_name"] = product_by_name
+        amazon["product_rating"] = product_rating
+        amazon["product_image"] = product_image
+        amazon["product_image_2"] = product_image_2
+        amazon["product_image_3"] = product_image_3
+        amazon["product_image_4"] = product_image_4
+        amazon["product_price"] = product_price
+        amazon["product_about"] = product_about
+        amazon["product_keywords"] = product_keywords
+        amazon["product_catlog"] = product_catlog
+        amazon["product_price_2"] = product_price_2
+        amazon["product_keywords_2"] = product_keywords_2
+        browser.quit()
+        return amazon
+
+
+####
